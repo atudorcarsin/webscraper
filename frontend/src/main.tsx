e50d40ac2897445fb592e8751f9b8516fe5e959f
Tudor Carsin <110934835+blue892@users.noreply.github.com>
2025-01-22 13:55:05 -0500

    Install Bootstrap and React-Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Index from './index.tsx'
+import Login from './login.tsx'
+import './main.scss'
+import './axiosConfig.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<Index/>} />
+          <Route path="login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
