e31269c57df8b1cb7597d821e77d7130ce231a07
Tudor Carsin <110934835+blue892@users.noreply.github.com>
2025-01-28 23:12:07 -0500

    Create webscraper models

--- backend/webscraper/models.py
+++ backend/webscraper/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=256, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=64)
+    url = models.URLField()
+    html_element_name = models.CharField(max_length=32)
+    filtering_options = models.JSONField()
+    data_retrieved = models.TextField(null=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
